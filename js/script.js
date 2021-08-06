3baac9b5e571cbe4d24c5d8bc8b2ed9223069e27
Yukihide Takahashi <yukihide99@gmail.com>
2021-08-06 15:42:52 +0800

    more stuff

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 // fix top nav
 $(document).ready(function() {
-    // var topnavPos = $("#topnav").offset().top;
-    // $(window).scroll(function() {
-    //     var scroll = $(window).scrollTop();
-    //     // console.log(scroll);
-    //     if (scroll > topnavPos / 2) {
-    //         $("#topnav").addClass("sticky");
-    //         console.log('class added')
-    //     }
-    // });
+    var topnavPos = $("#topnav").offset().top;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        // console.log(scroll);
+        if (scroll > topnavPos) {
+            $("#topnav").css({"position":"fixed", "top":"0px"});
+            // console.log('modified')
+        } else {
+            $("#topnav").css({"position":"absolute", "top":"75vh"})
+        }
+    });
 });
