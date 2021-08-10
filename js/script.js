d100e96e8e9d81f011b243436b3ad0f22fba14f5
Yukihide Takahashi <yukihide99@gmail.com>
2021-08-10 10:02:08 +0800

    added teams dropdown menus

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // fix top nav
 $(document).ready(function() {
+
+    // for top nav
     var topnavPos = $("#topnav").offset().top;
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
@@ -11,4 +13,41 @@ $(document).ready(function() {
             $("#topnav").css({"position":"absolute", "top":"75vh"})
         }
     });
+
+    // for teams list
+    var team_btn = $("#teams");
+    var teams_list = $("#teams-list");
+    team_btn.hover(
+        function(){
+            console.log("in function")
+            teams_list.css({
+                display: "block",
+                visibility: "visible",
+                opacity: 0.0
+            }
+            ).animate({
+                opacity: 1.0,
+            },500)
+        }, 
+        function(){});
+    teams_list.hover(
+        function(){},
+        function(){
+            console.log("out function")
+            
+            teams_list.animate({
+                opacity: 0.0,
+            },300)
+
+            setTimeout(function (){
+                teams_list.css({
+                    display: "none",
+                    visibility: "visible",
+                })
+            },300
+            )
+            
+        }
+    )
+
 });
